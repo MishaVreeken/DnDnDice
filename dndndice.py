8e7b462da43e178ccacd2f7498b3547b5cc34f2f
Misha Vreeken <mvreeken@nhzhnm33.laptops.hostnetbv.nl>
2021-09-23 14:42:30 +0200

    First draft

---
+++ dndndice.py
@@ -0,0 +1,9 @@
+# Asking what kind of dice they want to roll and the amount
+
+
+dice = input("What kind of dice do you want to trow? (how many sides")
+amount = input (f."How many of the d{dice} do you want to trow?")
+
+
+
+# calculating the score of the dice and returning the total
