9a6aeff027447a5b6e7178bb1ae7e6b6201c4fd9
Misha Vreeken <mvreeken@nhzhnm33.laptops.hostnetbv.nl>
2021-09-23 16:38:04 +0200

    Spelling mistakes fixed

--- dndndice.py
+++ dndndice.py
@@ -1,8 +1,7 @@
 from random import randint
 
 # Asking what kind of dice they want to roll and the amount
-
-dice = input("What kind of dice do you want to trow? (how many sides)\n")
+dice = input("What kind of dice do you want to throw? (how many sides)\n")
 amount = input (f"How many of the d{dice} do you want to throw?\n")
 
 # calculating the score of the dice and returning the total
