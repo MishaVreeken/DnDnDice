59b74d3b98a0b5c7dcb9c052e2773288e0cea4a2
Misha Vreeken <mvreeken@nhzhnm33.laptops.hostnetbv.nl>
2021-09-23 16:19:57 +0200

    Calculatie total of dice throws

--- dndndice.py
+++ dndndice.py
@@ -1,8 +1,14 @@
-# Asking what kind of dice they want to roll and the amount
+from random import randint
 
+# Asking what kind of dice they want to roll and the amount
 
 dice = input("What kind of dice do you want to trow? (how many sides)\n")
 amount = input (f"How many of the d{dice} do you want to trow?\n")
 
-
 # calculating the score of the dice and returning the total
+value_total = 0 
+for i in range(int(amount)):
+	value = randint(0, int(dice))
+	value_total += value
+	
+print(value_total)
