ff50d45cb2d5c65fb3238898b9f96fdbee5904bf
Misha Vreeken <mvreeken@nhzhnm33.laptops.hostnetbv.nl>
2021-09-23 16:30:13 +0200

    Oupit returns all values

--- dndndice.py
+++ dndndice.py
@@ -3,12 +3,15 @@ from random import randint
 # Asking what kind of dice they want to roll and the amount
 
 dice = input("What kind of dice do you want to trow? (how many sides)\n")
-amount = input (f"How many of the d{dice} do you want to trow?\n")
+amount = input (f"How many of the d{dice} do you want to throw?\n")
 
 # calculating the score of the dice and returning the total
 value_total = 0 
+value_list = []
 for i in range(int(amount)):
 	value = randint(0, int(dice))
+	value_list.append(value)
 	value_total += value
-	
-print(value_total)
+
+#printing the values	
+print(f"The value of the thrown dice are: { {*value_list} }\nWhich brings the total to {value_total}")
